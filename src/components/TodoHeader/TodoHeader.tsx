e2cebe1847e08378ee2a34e7e70cd98b0f1c7b63
Eyes0n <45257139+Eyes0n@users.noreply.github.com>
2021-08-24 17:07:26 +0900

    Revert "Feat: header layout"

--- src/components/TodoHeader/TodoHeader.tsx
+++ src/components/TodoHeader/TodoHeader.tsx
@@ -1,28 +1,13 @@
 import React from 'react';
 import TodoFilter from './TodoFilter/TodoFilter';
-import styled from 'styled-components';
 
 const TodoHeader: React.FC = () => {
   return (
-    <>
-      <Wrapper>
-        <h1>MODU ? TODO!</h1>
-      </Wrapper>
+    <div>
+      todoHeader
       <TodoFilter />
-    </>
+    </div>
   );
 };
 
 export default TodoHeader;
-
-const Wrapper = styled.div`
-  width: 100vw;
-  height: 100px;
-  background-color: #8ee5c2;
-  & h1 {
-    margin: 0;
-    color: white;
-    text-align: center;
-    line-height: 100px;
-  }
-`;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,1 @@
 export const todos = 'todos';
-
-export const PRIORITY_RANGE = ['-선택-', 'LOW', 'MEDIUM', 'HIGH'];
