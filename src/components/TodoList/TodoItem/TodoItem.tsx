588ef4007a1ec8e47ab9e378c14bde4a8c4ba6f3
Seokkitdo <sjd12329@gmail.com>
2021-08-24 14:51:58 +0900

    feat : todoitem layout

--- src/components/TodoList/TodoItem/TodoItem.tsx
+++ src/components/TodoList/TodoItem/TodoItem.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import styled from 'styled-components';
 const TodoItem: React.FC = () => {
-  return <div>todoItem</div>;
+  return (
+    <div>
+      <TodoLayout></TodoLayout>
+    </div>
+  );
 };
+
+const TodoLayout = styled.div`
+  width: 454px;
+  height: 89px;
+  border-radius: 68px;
+  background-color: skyblue;
+`;
 
 export default TodoItem;
