fb68b71da1dd33ad76c2a14b1a60b393b27e2a70
hyunchanpark <eye3570@gmail.com>
2021-08-24 23:09:40 +0900

    chore : mockdata수정

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ const App: React.FC = () => {
   const todos: ITodo[] = [
     { id: 1, task: 'first', priority: Priority.HIGH, status: Status.NOT_STARTED, deadLine: '1', createdAt: '1' },
     { id: 2, task: 'second', priority: Priority.HIGH, status: Status.IN_PROGRESS, deadLine: '2', createdAt: '2' },
-    { id: 3, task: 'third', priority: Priority.HIGH, status: Status.FINISHED, deadLine: '3', createdAt: '3' },
-    { id: 4, task: 'fourth', priority: Priority.HIGH, status: Status.NOT_STARTED, deadLine: '1', createdAt: '1' },
-    { id: 5, task: 'fifth', priority: Priority.HIGH, status: Status.IN_PROGRESS, deadLine: '2', createdAt: '2' },
-    { id: 6, task: 'sixth', priority: Priority.HIGH, status: Status.FINISHED, deadLine: '3', createdAt: '3' },
+    { id: 3, task: 'third', priority: Priority.MEDIUM, status: Status.FINISHED, deadLine: '3', createdAt: '3' },
+    { id: 4, task: 'fourth', priority: Priority.MEDIUM, status: Status.NOT_STARTED, deadLine: '1', createdAt: '1' },
+    { id: 5, task: 'fifth', priority: Priority.LOW, status: Status.IN_PROGRESS, deadLine: '2', createdAt: '2' },
+    { id: 6, task: 'sixth', priority: Priority.LOW, status: Status.FINISHED, deadLine: '3', createdAt: '3' },
   ];
   return (
     <TodoContainer>
