ec53fa7843bb119499a0cc62d2ed9c7dfbfa99bb
hyunchanpark <eye3570@gmail.com>
2021-08-26 00:23:53 +0900

    feat : filterOpeions 타입 생성

--- src/type.ts
+++ src/type.ts
@@ -27,3 +27,16 @@ export interface IEditTodo {
   status?: Status;
   deadLine?: Date;
 }
+
+export type OrderType = '' | 'DESC' | 'ASC';
+
+export interface IFilterOptions {
+  status: Status[]; // [Status.NOT_STARTED, Status.IN_PROGRESS, Status.FINISHED]
+  priority: Priority[]; // [Priority.LOW, Priority.MEDIUM, Priority.HIGH]
+  startDate?: Date | null;
+  endDate?: Date | null;
+  sortBy: string;
+  order: OrderType;
+}
+
+//Date는 deadLine 기준으로 비교
