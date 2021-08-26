33ee5c6d49adf0bdaff5aaf81faae8ecc4bfbd0c
hyunchanpark <eye3570@gmail.com>
2021-08-26 23:27:02 +0900

    feat : 정렬 타입 생성

--- src/type.ts
+++ src/type.ts
@@ -33,10 +33,13 @@ export type OrderType = '' | 'DESC' | 'ASC';
 export interface IFilterOptions {
   status: Status[]; // [Status.NOT_STARTED, Status.IN_PROGRESS, Status.FINISHED]
   priority: Priority[]; // [Priority.LOW, Priority.MEDIUM, Priority.HIGH]
-  startDate?: Date | null;
-  endDate?: Date | null;
-  sortBy: string;
-  order: OrderType;
+  startDate: Date | null;
+  endDate: Date | null;
 }
+
+export type SortOptions = {
+  sortBy: null | string;
+  order: OrderType;
+};
 
 //Date는 deadLine 기준으로 비교
