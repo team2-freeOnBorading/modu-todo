d65159fb3bde73017a41e90f778d4ab8f5686f8a
Park <pjane123@naver.com>
2021-08-27 01:13:08 +0900

    feat: DetailModal
    -ApplyButton 수정 handleTodo 콜백함수 추가

--- src/components/common/Modal/DetailModal/DetailModal.tsx
+++ src/components/common/Modal/DetailModal/DetailModal.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { ITodo, IEditTodo } from 'type';
+import { ITodo, IEditTodo, Priority, Status } from 'type';
 import Modal, { IModal } from '../Modal';
 import { dateToString } from 'utils/commons';
 import { useTodoAndDispatchContext } from 'context/TodoContext';
+import { ApplyButton, RadioButtonItem } from '../Button';
+import { PRIORITY_RANGE, STATUS_RANGE } from 'utils/constants';
+import ModalDatePicker from '../ModalDatePicker';
 
 interface IDetailModal extends IModal {
   item: ITodo;
@@ -15,8 +18,15 @@ const DetailModal: React.FC<IDetailModal> = ({ item, visible, onClose }) => {
   const [editTodo, setEditTodo] = useState<IEditTodo>(item);
   const { task, priority, status, deadLine } = editTodo;
 
+  const handleTodo = (key: string, value: string | Priority | Status | Date | null) => {
+    setEditTodo((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
   const onChangeTask = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEditTodo((prev) => ({ ...prev, task: e.target.value }));
+    handleTodo('task', e.target.value);
   };
 
   const applyTodo = () => {
@@ -26,22 +36,18 @@ const DetailModal: React.FC<IDetailModal> = ({ item, visible, onClose }) => {
   return (
     <Modal visible={visible} onClose={onClose}>
       <Wrapper>
+        <Label>TodoTask</Label>
         <TodoText onChange={onChangeTask} value={task} />
         <Label>Proiority</Label>
         <Value>{priority}</Value>
         <Label>Status</Label>
         <Value>{status}</Value>
-        {deadLine && (
-          <>
-            <Label>DeadLine</Label>
-            <Value>{dateToString(deadLine)}</Value>
-          </>
-        )}
+        {deadLine && <ModalDatePicker info='마감일' stateKey='deadLine' placeholderText='마감일 미설정' dateValue={deadLine} handleValue={handleTodo} />}
         <Label>CreatedAt</Label>
         <Value>{dateToString(createdAt)}</Value>
         <Label>UpdatedAt</Label>
         <Value>{dateToString(updatedAt)}</Value>
-        <button onClick={applyTodo}>apply</button>
+        <ApplyButton onClick={applyTodo}>apply</ApplyButton>
       </Wrapper>
     </Modal>
   );
@@ -54,7 +60,7 @@ const Wrapper = styled.div`
 const TodoText = styled.input``;
 
 const Label = styled.div`
-  font-size: 20px;
+  font-size: 17px;
   margin-bottom: 4px;
 `;
 
