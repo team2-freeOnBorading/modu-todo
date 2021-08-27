00c5de0721ab46dc459899ceaebdd084730a7a87
Park <pjane123@naver.com>
2021-08-27 22:28:00 +0900

    feat: ConfirmModal zindex 수정

--- src/components/common/Modal/ConfirmModal/ConfirmModal.tsx
+++ src/components/common/Modal/ConfirmModal/ConfirmModal.tsx
@@ -2,7 +2,7 @@ import Modal, { IModal } from '../Modal';
 
 const ConfirmModal: React.FC<IModal> = ({ visible, onClose }) => {
   return (
-    <Modal visible={visible} onClose={onClose}>
+    <Modal visible={visible} onClose={onClose} zIndex={200}>
       할일을 입력해주세요 📝
     </Modal>
   );
