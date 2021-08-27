26c636fcfa6b21ddbfff79ca46e16b067516579d
Park <pjane123@naver.com>
2021-08-27 16:59:00 +0900

    refactor: TodoItem 마다 각각 DetailModal 렌더하던 구조 수정

--- src/components/TodoKanban/TodoKanBan.tsx
+++ src/components/TodoKanban/TodoKanBan.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Status } from 'type';
+import { Status, ITodo, Priority } from 'type';
 import { useLoadStorage, useSaveStorage } from 'hooks/useStorage';
 import TodoList from './TodoList/TodoList';
+import useModal from 'hooks/useModal';
+import DetailModal from 'components/common/Modal/DetailModal';
+
+const defaultModal: ITodo = {
+  id: 1,
+  task: '',
+  priority: Priority.LOW,
+  status: Status.NOT_STARTED,
+  deadLine: new Date(),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
 
 const TodoKanBan: React.FC = () => {
   useLoadStorage();
   useSaveStorage();
+  const [modalVisible, openModal, closeModal] = useModal(false);
+  const [detailTodo, setDetailTodo] = useState<ITodo | null>(null);
+
+  const openDetail = (item: ITodo) => {
+    setDetailTodo(item);
+    openModal();
+  };
 
   return (
     <TodoKanBanContainer>
-      <TodoList status={Status.NOT_STARTED} />
-      <TodoList status={Status.IN_PROGRESS} />
-      <TodoList status={Status.FINISHED} />
+      <TodoList status={Status.NOT_STARTED} openDetail={openDetail} />
+      <TodoList status={Status.IN_PROGRESS} openDetail={openDetail} />
+      <TodoList status={Status.FINISHED} openDetail={openModal} />
+      <DetailModal visible={modalVisible} onClose={closeModal} item={detailTodo || defaultModal} />
     </TodoKanBanContainer>
   );
 };
